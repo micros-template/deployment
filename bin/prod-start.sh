3f7cabf0029c3d7b594f09e9de9c8b695c510b68
daffadon <daffaputranarendra9@gmail.com>
2025-07-14 16:47:39 +0700

    chore(ci): change ci script

--- bin/prod-start.sh
+++ bin/prod-start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "init"
 ./bin/prod-init.sh
 
 export COMPOSE_BAKE=true
 
+echo "starting"
 cd ./docker
 docker compose --env-file ../.env -p dropboks \
   -f docker-compose.gateway.yml \
