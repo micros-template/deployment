348abe65f1c720412a1d461d2467b602612962ec
daffadon <daffaputranarendra9@gmail.com>
2025-07-15 08:06:44 +0700

    chore(ci): change ci script

--- bin/prod-start.sh
+++ bin/prod-start.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-echo "init"
 ./bin/prod-init.sh
 
 export COMPOSE_BAKE=true
 
-echo "starting"
 cd ./docker
 docker compose --env-file ../.env -p dropboks \
+  -f docker-compose.gateway.yml \
   -f docker-compose.gateway-prometheus-exp.yml \
   -f docker-compose.auth-svc.yml \
   -f docker-compose.user-svc.yml \
   -f docker-compose.file-svc.yml \
   -f docker-compose.notification-svc.yml \
   up --wait -d --build
-
-# -f docker-compose.gateway.yml \
