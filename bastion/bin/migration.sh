121b8e6dc8c648dc46736a50053f04efd491ebf3
daffadon <daffaputranarendra9@gmail.com>
2025-07-15 09:32:28 +0700

    chore(ci): fix bastion migration host

--- bastion/bin/migration.sh
+++ bastion/bin/migration.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "migration starting"
 for databasename in auth_db user_db; do
   migrate -database "postgres://${DB_USER}:${DB_PASSWORD}@${databasename}:${DB_PORT}/${DB_NAME}?sslmode=${DB_SSLMODE}" -path /init up
 done
