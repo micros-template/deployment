4ee0d813219b5d4324506705f85cbb499efe92ba
daffadon <daffaputranarendra9@gmail.com>
2025-07-15 09:51:26 +0700

    chore(ci): debug bastion

--- bastion/bin/migration.sh
+++ bastion/bin/migration.sh
@@ -2,5 +2,5 @@
 
 echo "migration starting"
 for databasename in auth_db user_db; do
-  migrate -database "postgres://${DB_USER}:${DB_PASSWORD}@${databasename}:${DB_PORT}/${DB_NAME}?sslmode=${DB_SSLMODE}" -path /init up
+  migrate -database "postgres://${DB_USER}:${DB_PASSWORD}@${databasename}:${DB_PORT}/${DB_NAME}?sslmode=${DB_SSLMODE}" -path /init/migrations up
 done
